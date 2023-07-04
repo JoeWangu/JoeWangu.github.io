52e4d27faf6a4f0061215ab57a8c495bd7d6bd95
wangu <joewangu2021@outlook.com>
2023-07-04 19:52:38 +0300

    04-July-2023 update reviews and featured sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,117 @@
 $(document).ready(function () {
-(function ($) { // Begin jQuery
-    $(function () { // DOM ready
-        // If a link has a dropdown, add sub menu toggle.
-        $('nav ul li a:not(:only-child)').click(function (e) {
-            $(this).siblings('.nav-dropdown').toggle();
-            // Close one dropdown when selecting another
-            $('.nav-dropdown').not($(this).siblings()).hide();
-            e.stopPropagation();
-        });
-        // Clicking away from dropdown will remove the dropdown class
-        $('html').click(function () {
-            $('.nav-dropdown').hide();
-        });
-        // Toggle open and close nav styles on click
-        $('#nav-toggle').click(function () {
-            $('nav ul').slideToggle();
-        });
-        // Hamburger to X toggle
-        $('#nav-toggle').on('click', function () {
-            this.classList.toggle('active');
-        });
-    }); // end DOM ready
-})(jQuery); // end jQuery
+    //  -------------------------------------------------------------------------------------------------------------------
+    //                                       NAVBAR SECTION JS
+    //  -------------------------------------------------------------------------------------------------------------------- //
+    (function ($) { // Begin jQuery
+        $(function () { // DOM ready
+            // If a link has a dropdown, add sub menu toggle.
+            $('nav ul li a:not(:only-child)').click(function (e) {
+                $(this).siblings('.nav-dropdown').toggle();
+                // Close one dropdown when selecting another
+                $('.nav-dropdown').not($(this).siblings()).hide();
+                e.stopPropagation();
+            });
+            // Clicking away from dropdown will remove the dropdown class
+            $('html').click(function () {
+                $('.nav-dropdown').hide();
+            });
+            // Toggle open and close nav styles on click
+            $('#nav-toggle').click(function () {
+                $('nav ul').slideToggle();
+            });
+            // Hamburger to X toggle
+            $('#nav-toggle').on('click', function () {
+                this.classList.toggle('active');
+            });
+        }); // end DOM ready
+    })(jQuery); // end jQuery
+
+    //  -------------------------------------------------------------------------------------------------------------------
+    //                                       SLIDESHOW JS
+    //  -------------------------------------------------------------------------------------------------------------------- //
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    let intervalId = setInterval(function () {
+        plusSlides(1);
+    }, 5000);
+
+    function plusSlides(n) {
+        showSlides((slideIndex += n));
+    }
+
+    function currentSlide(n) {
+        showSlides((slideIndex = n));
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = $(".mySlides");
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.hide();
+        slides.removeClass("active");
+        slides.eq(slideIndex - 1).show().addClass("active");
+
+        let dots = $(".dot");
+        dots.removeClass("active");
+        dots.eq(slideIndex - 1).addClass("active");
+    }
+
+    let slideshowContainer = $(".image").eq(0);
+    slideshowContainer.mouseover(stopSlideshow);
+    slideshowContainer.mouseout(startSlideshow);
+
+    function stopSlideshow() {
+        clearInterval(intervalId);
+    }
+
+    function startSlideshow() {
+        intervalId = setInterval(function () {
+            plusSlides(1);
+        }, 5000);
+    }
+
+    //  -------------------------------------------------------------------------------------------------------------------
+    //                                       FADE IN ANIMATION FOR CARDS SECTION
+    //  -------------------------------------------------------------------------------------------------------------------- //
+    window.addEventListener('scroll', function () {
+        var elements = document.querySelectorAll('.fade-in-animation');
+        for (var i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var position = element.getBoundingClientRect().top;
+            var windowHeight = window.innerHeight;
+
+            if (position < windowHeight) {
+                element.classList.add('fade-in');
+            }
+            // else {
+            //     element.classList.remove('fade-in')
+            // }
+        }
+    });
+
+    //  -------------------------------------------------------------------------------------------------------------------
+    //                                       FEATURED SECTION JS
+    //  -------------------------------------------------------------------------------------------------------------------- //
+    $('.slide').click(function () {
+        $('.slide').removeClass('active'); // Remove 'active' class from all slides
+        $(this).addClass('active'); // Add 'active' class to the clicked slide
+
+        var imageSrc = $(this).attr('src');
+        var imageAlt = $(this).attr('alt');
+        $('#main-image').attr('src', imageSrc);
+        $('#main-image').attr('alt', imageAlt);
+    });
+});
+
 
 //  -------------------------------------------------------------------------------------------------------------------
-//                                       slideshow
+//                                       SLIDESHOW WITHOUT JQUERY
 //  -------------------------------------------------------------------------------------------------------------------- //
 // let slideIndex = 1;
 // showSlides(slideIndex);
@@ -78,72 +165,9 @@ $(document).ready(function () {
 //     }
 // }
 
-//  jquery javascript
-let slideIndex = 1;
-showSlides(slideIndex);
-
-let intervalId = setInterval(function () {
-    plusSlides(1);
-}, 5000);
-
-function plusSlides(n) {
-    showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-    showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-    let i;
-    let slides = $(".mySlides");
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    slides.hide();
-    slides.removeClass("active");
-    slides.eq(slideIndex - 1).show().addClass("active");
-
-    let dots = $(".dot");
-    dots.removeClass("active");
-    dots.eq(slideIndex - 1).addClass("active");
-}
-
-let slideshowContainer = $(".image").eq(0);
-slideshowContainer.mouseover(stopSlideshow);
-slideshowContainer.mouseout(startSlideshow);
-
-function stopSlideshow() {
-    clearInterval(intervalId);
-}
-
-function startSlideshow() {
-    intervalId = setInterval(function () {
-        plusSlides(1);
-    }, 5000);
-}
-
-// fade in animation for cards section
-window.addEventListener('scroll', function () {
-    var elements = document.querySelectorAll('.fade-in-animation');
-    for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var position = element.getBoundingClientRect().top;
-        var windowHeight = window.innerHeight;
-
-        if (position < windowHeight) {
-            element.classList.add('fade-in');
-        }
-        // else {
-        //     element.classList.remove('fade-in')
-        // }
-    }
-});
-
-// featured section js
+//  -------------------------------------------------------------------------------------------------------------------
+//                                       FEATURED SECTION JS ANOTHER ITERATION
+//  -------------------------------------------------------------------------------------------------------------------- //
 // function changeMainImage(image) {
 //     var mainImage = document.getElementById("main-image");
 //     mainImage.src = image.src;
@@ -158,15 +182,3 @@ window.addEventListener('scroll', function () {
 //     $('#main-image').attr('alt', imageAlt);
 // });
 // });
-
-
-    $('.slide').click(function () {
-        $('.slide').removeClass('active'); // Remove 'active' class from all slides
-        $(this).addClass('active'); // Add 'active' class to the clicked slide
-
-        var imageSrc = $(this).attr('src');
-        var imageAlt = $(this).attr('alt');
-        $('#main-image').attr('src', imageSrc);
-        $('#main-image').attr('alt', imageAlt);
-    });
-});
