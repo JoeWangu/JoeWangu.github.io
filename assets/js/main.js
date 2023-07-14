ea747147735d7603bf37beb849539b780dad4f8d
wangu <joewangu2021@outlook.com>
2023-07-14 23:36:57 +0300

    14-July-2023 color and nav updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,40 @@
 // $(document).ready(function () {
 
 // });
-let navToggle = document.querySelector(".nav__toggle");
-let navWrapper = document.querySelector(".nav__wrapper");
 
-navToggle.addEventListener("click", function () {
-    if (navWrapper.classList.contains("active")) {
-        this.setAttribute("aria-expanded", "false");
-        this.setAttribute("aria-label", "menu");
-        navWrapper.classList.remove("active");
-    } else {
-        navWrapper.classList.add("active");
-        this.setAttribute("aria-label", "close menu");
-        this.setAttribute("aria-expanded", "true");
-        searchForm.classList.remove("active");
-        searchToggle.classList.remove("active");
-    }
-});
+// let navToggle = document.querySelector(".nav__toggle");
+// let navWrapper = document.querySelector(".nav__wrapper");
 
-let searchToggle = document.querySelector(".search__toggle");
-let searchForm = document.querySelector(".search__form");
+// navToggle.addEventListener("click", function () {
+//     if (navWrapper.classList.contains("active")) {
+//         this.setAttribute("aria-expanded", "false");
+//         this.setAttribute("aria-label", "menu");
+//         navWrapper.classList.remove("active");
+//     } else {
+//         navWrapper.classList.add("active");
+//         this.setAttribute("aria-label", "close menu");
+//         this.setAttribute("aria-expanded", "true");
+//         searchForm.classList.remove("active");
+//         searchToggle.classList.remove("active");
+//     }
+// });
 
-searchToggle.addEventListener("click", showSearch);
+// let searchToggle = document.querySelector(".search__toggle");
+// let searchForm = document.querySelector(".search__form");
 
-function showSearch() {
-    searchForm.classList.toggle("active");
-    searchToggle.classList.toggle("active");
+// searchToggle.addEventListener("click", showSearch);
 
-    navToggle.setAttribute("aria-expanded", "false");
-    navToggle.setAttribute("aria-label", "menu");
-    navWrapper.classList.remove("active");
+// function showSearch() {
+//     searchForm.classList.toggle("active");
+//     searchToggle.classList.toggle("active");
 
-    if (searchToggle.classList.contains("active")) {
-        searchToggle.setAttribute("aria-label", "Close search");
-    } else {
-        searchToggle.setAttribute("aria-label", "Open search");
-    }
-}
+//     navToggle.setAttribute("aria-expanded", "false");
+//     navToggle.setAttribute("aria-label", "menu");
+//     navWrapper.classList.remove("active");
+
+//     if (searchToggle.classList.contains("active")) {
+//         searchToggle.setAttribute("aria-label", "Close search");
+//     } else {
+//         searchToggle.setAttribute("aria-label", "Open search");
+//     }
+// }
