1fd0f0ab18a55d0fa224aba4395c5dc6a52e568f
wangu <joewangu2021@outlook.com>
2023-07-17 16:05:11 +0300

    17-July-2023 navbar now functional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,53 @@
-// $(document).ready(function () {
-//     // If a link has a dropdown, add sub menu toggle.
-//     $('.dropdown a:not(:only-child)').click(function (e) {
-//         // $('#cn-overlay').removeClass('on-overlay');
-//         $(this).siblings('.hide').slideToggle();
-//         if ($(this).siblings('.hide').css('display')==='none') {
-//                 $('#cn-overlay').removeClass('on-overlay'); // Hide the overlay
-//         }
-//         else {
-//                 $('#cn-overlay').addClass('on-overlay'); // Show the overlay
-//             }
-//         // Close one dropdown when selecting another
-//         $('.hide').not($(this).siblings()).hide();
-//         e.stopPropagation();
-//     });
+$(document).ready(function () {
+    // -----------------------------------------------------------------------------------------------------------
+    // ------------------------------------------- TOGGLE THE BOTTOM NAVBAR -----------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+    $('.dropdown a:not(:only-child)').click(function () {
+        var link = $(this);
+        var icon = link.find('i');
+        $('#cn-overlay').addClass('on-overlay');
+        icon.removeClass('fa-angle-down').addClass('fa-angle-up');
+        $(this).siblings('.hide').slideToggle(400, function () {
+            if (!$('.dropdown .hide').is(':visible')) {
+                $('#cn-overlay').removeClass('on-overlay');
+                icon.removeClass('fa-angle-up').addClass('fa-angle-down');
+            }
+        });
+        $('.hide').not($(this).siblings()).hide();
+        $('.dropdown i').not(icon).removeClass('fa-angle-up').addClass('fa-angle-down');
+        return false;
+    });
 
-//     // Clicking away from dropdown will remove the dropdown class
-//     $('html').click(function () {
-//         $('.hide').hide();
-//         $('#cn-overlay').removeClass('on-overlay');// Hide the overlay
-//     })
-// });
+    // Clicking away from dropdown will remove the dropdown class
+    $(document).on('click', function (e) {
+        if (!$(e.target).closest('.dropdown').length) {
+            $('.hide').hide();
+            $('#cn-overlay').removeClass('on-overlay'); // Hide the overlay
+            $('.dropdown i').removeClass('fa-angle-up').addClass('fa-angle-down');
+        }
+    });
+
+    // -----------------------------------------------------------------------------------------------------------
+    // ------------------------------------------- ADJUST NAVBAR IN SMALLER SCREENS -----------------------------------------
+    // -----------------------------------------------------------------------------------------------------------
+    $(window).on('resize', checkPosition);
+
+    function checkPosition() {
+        if ($(window).innerWidth() < 810) {
+            $('.top-nav-wrapper .nav-link-wrapper:first').empty().html(`<input  type="checkbox" id="burger-toggle">
+                <label for= "burger-toggle" class= "burger-menu">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+                </label > `);        
+        } else {
+            $('.top-nav-wrapper .nav-link-wrapper:first').empty().html(`<a href="#!">
+            <i class="fa-solid fa-home" aria-hidden="true"></i>
+            <p>Home</p>
+        </a>`);        
+        }
+    }
 
 
-// toggle bottom navbar
-// (function () {
-    // let overlay = document.getElementById('cn-overlay');
-    // let dropdownItems = document.querySelectorAll('.dropdown a:not(:only-child)')
-    // let hide = document.querySelector('.hide');
-    // let icon = document.querySelector('.fa-solid')
-    // let wrapper = document.getElementById('dropdown-wrapper')
-    // let open = false
-
-    // dropdownItems.forEach(function (link) {
-    //     link.addEventListener('click', function (e) {
-            
-    //     })
-    // });
-
-    // dropdownItems.forEach(function (dropdown) {
-    //     dropdown.addEventListener('click', handle, false)
-    // })
-    // dropdown.addEventListener('click', handle, false);
-// })();
+    checkPosition();
+});
--- assets/js/side.js
+++ assets/js/side.js
@@ -2,160 +2,43 @@
 // ------------------------------------------- TOGGLE BETWEEN THEMES -----------------------------------------
 // -----------------------------------------------------------------------------------------------------------
 $(document).ready(function () {
-const themeIcon = $("#themeIcon");
-const tooltipText = $("#themeIcon .tooltip-text");
+    const themeIcon = $("#themeIcon");
+    const tooltipText = $("#themeIcon .tooltip-text");
 
-// Set the theme to light only if it hasn't been set before
-if (!localStorage.getItem('theme')) {
-    localStorage.setItem('theme', 'light');
-}
-
-// Retrieve the saved theme from localStorage and apply it to the page
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-    $('html').attr('data-theme', savedTheme);
-    if (savedTheme === 'dark') {
-        themeIcon.removeClass('fa-solid fa-sun').addClass('fa-solid fa-moon');
-        tooltipText.text('Switch to light mode');
-    } else {
-        themeIcon.removeClass('fa-solid fa-moon').addClass('fa-solid fa-sun');
-        tooltipText.text('Switch to dark mode');
-    }
-}
-
-themeIcon.on('click', () => {
-    const currentTheme = $('html').attr('data-theme');
-    if (currentTheme === 'light') {
-        $('html').attr('data-theme', 'dark');
-        themeIcon.removeClass('fa-sun').addClass('fa-moon');
-        tooltipText.text('Switch to light mode');
-        localStorage.setItem('theme', 'dark');
-    } else {
-        $('html').attr('data-theme', 'light');
-        themeIcon.removeClass('fa-moon').addClass('fa-sun');
-        tooltipText.text('Switch to dark mode');
+    // Set the theme to light only if it hasn't been set before
+    if (!localStorage.getItem('theme')) {
         localStorage.setItem('theme', 'light');
     }
-});
+
+    // Retrieve the saved theme from localStorage and apply it to the page
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        $('html').attr('data-theme', savedTheme);
+        if (savedTheme === 'dark') {
+            themeIcon.removeClass('fa-solid fa-sun').addClass('fa-solid fa-moon');
+            tooltipText.text('Switch to light mode');
+        } else {
+            themeIcon.removeClass('fa-solid fa-moon').addClass('fa-solid fa-sun');
+            tooltipText.text('Switch to dark mode');
+        }
+    }
+
+    themeIcon.on('click', () => {
+        const currentTheme = $('html').attr('data-theme');
+        if (currentTheme === 'light') {
+            $('html').attr('data-theme', 'dark');
+            themeIcon.removeClass('fa-sun').addClass('fa-moon');
+            tooltipText.text('Switch to light mode');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            $('html').attr('data-theme', 'light');
+            themeIcon.removeClass('fa-moon').addClass('fa-sun');
+            tooltipText.text('Switch to dark mode');
+            localStorage.setItem('theme', 'light');
+        }
+    });
 
 });
 // -----------------------------------------------------------------------------------------------------------
 // -------------------------------------------  -----------------------------------------
 // -----------------------------------------------------------------------------------------------------------
-
-// // If a link has a dropdown, add sub menu toggle.
-// $('nav ul li a:not(:only-child)').click(function (e) {
-//     $(this).siblings('.nav-dropdown').toggle();
-//     // Close one dropdown when selecting another
-//     $('.nav-dropdown').not($(this).siblings()).hide();
-//     e.stopPropagation();
-// });
-// // Clicking away from dropdown will remove the dropdown class
-// $('html').click(function () {
-//     $('.nav-dropdown').hide();
-// });
-// // Toggle open and close nav styles on click
-// $('#nav-toggle').click(function () {
-//     $('nav ul').slideToggle();
-// });
-// // Hamburger to X toggle
-// $('#nav-toggle').on('click', function () {
-//     this.classList.toggle('active');
-
-
-
-// var dropdownOpen = false; // Variable to keep track of the dropdown state
-
-// // If a link has a dropdown, add sub menu toggle.
-// $('.dropdown a:not(:only-child)').click(function (e) {
-//     $(this).siblings('.hide').slideToggle();
-//     if (dropdownOpen) {
-//         $('#cn-overlay').removeClass('on-overlay'); // Hide the overlay
-//         dropdownOpen = false;
-//     } else {
-//         $('#cn-overlay').addClass('on-overlay'); // Show the overlay
-//         dropdownOpen = true;
-//     }
-//     // Close one dropdown when selecting another
-//     $('.hide').not($(this).siblings()).hide();
-//     e.stopPropagation();
-// });
-// // Clicking away from dropdown will remove the dropdown class
-// $('html').click(function () {
-//     $('.hide').hide();
-//     $('#cn-overlay').removeClass('on-overlay'); // Hide the overlay
-//     dropdownOpen = false;
-// })
-
-
-// $(document).ready(function () {
-//     // If a link has a dropdown, add sub menu toggle.
-//     // var dropdownOpen = false; // Variable to keep track of the dropdown state
-//     $('.dropdown a:not(:only-child)').click(function (e) {
-//         $(this).siblings('.hide').slideToggle();
-//         if (dropdownOpen) {
-//             $('#cn-overlay').removeClass('on-overlay'); // Hide the overlay
-//             dropdownOpen = false;
-//         } else {
-//             $('#cn-overlay').addClass('on-overlay'); // Show the overlay
-//             dropdownOpen = true;
-//         }
-//         // Close one dropdown when selecting another
-//         $('.hide').not($(this).siblings()).hide();
-//         e.stopPropagation();
-//     });
-//     // Clicking away from dropdown will remove the dropdown class
-//     $('html').click(function () {
-//         $('.hide').hide();
-//         $('#cn-overlay').removeClass('on-overlay');// Hide the overlay
-//         dropdownOpen = false;
-//     })
-// });
-
-// $(document).ready(function () {
-//     // If a link has a dropdown, add sub menu toggle.
-//     $('.dropdown a:not(:only-child)').click(function (e) {
-//         // $('#cn-overlay').removeClass('on-overlay');
-//         $(this).siblings('.hide').slideToggle().toggleClass();
-//         // Close one dropdown when selecting another
-//         $('.hide').not($(this).siblings()).hide();
-//         e.stopPropagation();
-//     });
-
-//     // Clicking away from dropdown will remove the dropdown class
-//     $(document).on('click', function (e) {
-//         if (!$(e.target).closest('.dropdown').length) {
-//             $('.hide').hide();
-//             $('#cn-overlay').removeClass('on-overlay');// Hide the overlay
-//         }
-//     });
-// });
-
-var isDropdownOpen = false;
-
-$(document).ready(function () {
-    // If a link has a dropdown, add sub menu toggle.
-    $('.dropdown a:not(:only-child)').click(function (e) {
-        $(this).siblings('.hide').slideToggle();
-        isDropdownOpen = !isDropdownOpen
-        if (isDropdownOpen) {
-            $('#cn-overlay').addClass('on-overlay'); // Show the overlay
-        }
-        else {
-            $('#cn-overlay').removeClass('on-overlay'); // Hide the overlay
-        }
-        // Close one dropdown when selecting another
-        $('.hide').not($(this).siblings()).hide();
-        e.stopPropagation();
-    });
-
-    // Clicking away from dropdown will remove the dropdown class
-    $(document).on('click', function (e) {
-        if (!$(e.target).closest('.dropdown').length) {
-            $('.hide').hide();
-            $('#cn-overlay').removeClass('on-overlay');// Hide the overlay
-            isDropdownOpen = !isDropdownOpen
-        }
-    });
-});
-
